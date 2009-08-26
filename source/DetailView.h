31ff417810e673b1fd1f74e7ef923a7d0b2cdbd6
titilima <titilima@1413edd6-91fc-11de-b1fe-fbc52978f058>
2009-08-26 04:56:19 +0000

    1.00.023

---
+++ source/DetailView.h
@@ -0,0 +1,104 @@
+///////////////////////////////////////////////////////////////////////////////
+// 文件名：  DetailView.h
+// 创建时间：2007-11-2
+// 作者：    李马
+// 版权所有：Titi Studio (?) 2001-2007
+//-----------------------------------------------------------------------------
+// 说明：    符号详细信息类
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef PDBEXP_DETAILVIEW
+#define PDBEXP_DETAILVIEW
+
+#pragma once
+
+#include <PDLWindow.h>
+#include <PDLCom.h>
+#include <ExDisp.h>
+#include <MsHTML.h>
+#include <MsHtmHst.h>
+#include <dia2.h>
+#include <cvconst.h>
+
+class CEventHandler
+{
+public:
+    virtual IDiaSymbol* OnSymbolChange(LPCWSTR pszName);
+    virtual void OnNavigateComplete(void);
+};
+
+class CDetailView : public LAxCtrl, public IDocHostUIHandler
+{
+public:
+    CDetailView(void);
+    BOOL Create(__in LPCRECT lpRect, __in HWND hWndParent, __in UINT nID,
+        __in LPCWSTR pszTemplate);
+public:
+    void AddText(__in LPCWSTR pszText);
+    void Clear(void);
+    void Copy(void);
+    void CopyAll(void);
+    IDiaSymbol* DetachCurrentSymbol(void);
+    void EnableHyperLink(__in BOOL bEnable);
+    IDiaSymbol* GetCurrentSymbol(void);
+    BOOL GetText(__out LStringA* pStr);
+    void SetCurrentSymbol(__in IDiaSymbol* pCurSymbol);
+    BOOL SetEventHandler(__in CEventHandler* pEventHandler);
+private:
+    int OnCreate(LPCREATESTRUCT lpCreateStruct, BOOL& bHandled);
+    void OnDestroy(BOOL& bHandled);
+    void OnSize(UINT nType, int cx, int cy, BOOL& bHandled);
+private:
+    // IUnknown
+    HRESULT STDMETHODCALLTYPE QueryInterface(__in REFIID iid,
+        __out LPVOID* ppvObject);
+    ULONG STDMETHODCALLTYPE AddRef(void);
+    ULONG STDMETHODCALLTYPE Release(void);
+private:
+    // IDispatch
+    HRESULT STDMETHODCALLTYPE Invoke(__in DISPID dispIdMember, __in REFIID riid,
+        __in LCID lcid, __in WORD wFlags, __inout DISPPARAMS *pDispParams,
+        __out VARIANT *pVarResult, __out EXCEPINFO *pExcepInfo,
+        __out UINT *puArgErr);
+    void BeforeNavigate2(__in IDispatch* pDisp, __in VARIANT* URL,
+        __in VARIANT* Flags, __in VARIANT* TargetFrameName,
+        __in VARIANT* PostData, __in VARIANT* Headers,
+        __inout VARIANT_BOOL* Cancel);
+    void NavigateComplete2(__in IDispatch* pDisp, __in VARIANT* URL);
+private:
+    // IDocHostUIHandler
+    HRESULT STDMETHODCALLTYPE ShowContextMenu(__in DWORD dwID, __in POINT *ppt,
+        __in IUnknown *pcmdtReserved, __in IDispatch *pdispReserved);
+    HRESULT STDMETHODCALLTYPE GetHostInfo(__inout DOCHOSTUIINFO *pInfo);
+    HRESULT STDMETHODCALLTYPE ShowUI(__in DWORD dwID,
+        __in IOleInPlaceActiveObject *pActiveObject,
+        __in IOleCommandTarget *pCommandTarget, __in IOleInPlaceFrame *pFrame,
+        __in IOleInPlaceUIWindow *pDoc);
+    HRESULT STDMETHODCALLTYPE HideUI(void);
+    HRESULT STDMETHODCALLTYPE UpdateUI(void);
+    HRESULT STDMETHODCALLTYPE EnableModeless(__in BOOL fEnable);
+    HRESULT STDMETHODCALLTYPE OnDocWindowActivate(__in BOOL fActivate);
+    HRESULT STDMETHODCALLTYPE OnFrameWindowActivate(__in BOOL fActivate);
+    HRESULT STDMETHODCALLTYPE ResizeBorder(__in LPCRECT prcBorder,
+        __in IOleInPlaceUIWindow *pUIWindow, __in BOOL fRameWindow);
+    HRESULT STDMETHODCALLTYPE TranslateAccelerator(__in LPMSG lpMsg,
+        __in const GUID *pguidCmdGroup, __in DWORD nCmdID);
+    HRESULT STDMETHODCALLTYPE GetOptionKeyPath(__out LPOLESTR *pchKey,
+        __in DWORD dw);
+    HRESULT STDMETHODCALLTYPE GetDropTarget(__in IDropTarget *pDropTarget,
+        __out IDropTarget **ppDropTarget);
+    HRESULT STDMETHODCALLTYPE GetExternal(__out IDispatch **ppDispatch);
+    HRESULT STDMETHODCALLTYPE TranslateUrl(__in DWORD dwTranslate,
+        __in OLECHAR *pchURLIn, __out OLECHAR **ppchURLOut);
+    HRESULT STDMETHODCALLTYPE FilterDataObject(__in IDataObject *pDO,
+        __out IDataObject **ppDORet);
+private:
+    LComPtr<IWebBrowser2>   m_pWB2;
+    LComPtr<IHTMLDocument2> m_pDoc;
+    LComPtr<IHTMLElement>   m_pBody;
+    IDiaSymbol             *m_pCurSymbol;
+    CEventHandler          *m_pEventHandler;
+    BOOL                    m_bEnable;
+};
+
+#endif PDBEXP_DETAILVIEW
